0ef5c33e779cb4e48a463c08f1390e67cc970660
sbartkowski <stanislawbartkowski@gmail.com>
2020-11-20 20:46:38 +0100

    first draft

---
+++ db2insert.sql
@@ -0,0 +1,15 @@
+import from data/accountholder.csv of del replace into account_holders;
+
+import from data/bankaccounts.csv of del replace into bank_accounts;
+
+import from data/bankclients.csv of del replace into bank_clients;
+
+import from data/bankcustomers.csv of del replace into bank_customers;
+
+import from data/bankwrkexsalary.csv of del replace into BANK_WRKEX_SALARY;
+
+import from data/checkingaccounts.csv of del replace into CHECKING_ACCOUNTS;
+
+import from data/savingaccounts.csv of del replace into SAVINGS_ACCOUNTS;
+
+
