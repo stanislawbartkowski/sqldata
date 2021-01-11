061f25f1e11235de5e7c0aaedd9c1515dc6dee76
sb <sb@banquets1.fyre.ibm.com>
2021-01-10 16:00:27 -0800

    hive

---
+++ hiveinsert.sql
@@ -0,0 +1,15 @@
+load data inpath '/tmp/accountholder.csv' overwrite into table account_holders;
+
+load data inpath '/tmp/bankaccounts.csv' overwrite into table bank_accounts;
+
+load data inpath '/tmp/bankclients.csv' overwrite into table bank_clients;
+
+load data inpath '/tmp/bankcustomers.csv' overwrite into table bank_customers;
+
+load data inpath '/tmp/bankwrkexsalary.csv'  overwrite into table BANK_WRKEX_SALARY;
+
+load data inpath '/tmp/checkingaccounts.csv' overwrite into table CHECKING_ACCOUNTS;
+
+load data inpath '/tmp/savingaccounts.csv' overwrite into table SAVINGS_ACCOUNTS;
+
+
